de5cd5cca0240878ea8e84c48ad3d514a4926b33
Katchapon Poolpipat <katchaponpoolpipat@Hagane.local>
2022-02-17 15:54:24 +0700

    update repayment schedule helper test

--- tests/Unit/Helpers/RepyamentScheduleHelperTest.php
+++ tests/Unit/Helpers/RepyamentScheduleHelperTest.php
@@ -11,12 +11,15 @@ class RepaymentScheduleTest extends TestCase
 {
     use DatabaseMigrations;
 
-    public function testGenerateRepaymentSchedules()
+    public function test_generate_repayment_schedules()
     {
         $loan = Loan::factory()->create();
 
         $repaymentSchedules = RepaymentScheduleHelper::generateRepaymentSchedules($loan);
 
-        $this->assertEquals($loan->loan_term * 12, count($repaymentSchedules));
+        $this->assertEquals($loan->loan_term, count($repaymentSchedules));
+        $this->assertEquals(879.16, round($repaymentSchedules[0]['payment_amount'], 2));
+        $this->assertEquals(0, $repaymentSchedules[11]['balance']);
+        $this->assertEquals(12, $repaymentSchedules[11]['payment_no']);
     }
 }
